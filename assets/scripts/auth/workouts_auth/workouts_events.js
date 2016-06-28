0d06f8baaae7f792024f624a58eb68c6797d7e81
andreasantarl <andrea.santarl@gmail.com>
2016-06-28 10:05:19 -0400

    Cleans up comments
    Comments removed

--- assets/scripts/auth/workouts_auth/workouts_events.js
+++ assets/scripts/auth/workouts_auth/workouts_events.js
@@ -12,10 +12,6 @@ const onCreateWorkout = (event) => {
   .fail(ui.createWorkoutFailure);
 };
 
-// const addClassClicked = (event) => {
-//   $(event.target).addClass('clicked');
-// };
-
 const onSeeWorkouts = (event) => {
   event.preventDefault();
   api.seeWorkouts()
@@ -23,32 +19,16 @@ const onSeeWorkouts = (event) => {
   .fail(ui.seeWorkoutsFailure);
 };
 
-
 const onDeleteWorkout = (event) => {
-  //debugger;
   event.preventDefault();
   let buttonId = $(event.target).attr('data-id');
   api.deleteWorkout(buttonId)
   .done(ui.deleteWorkoutSuccess)
   .fail(ui.deleteWorkoutFailure);
   onSeeWorkouts(event);
-
-//update app.user
-
-  // $('.delete-workout').on('click', function(event){
-  //   event.preventDefault();
-  //   let buttonId = $(event.target).attr('data-id');
-  //   api.deleteWorkout(buttonId)
-  //   .done(deleteWorkoutSuccess)
-  //   .fail(deleteWorkoutFailure);
-  // });
 };
 
 const onEditWorkout = (event) => {
-  // event.preventDefault();
-  // console.log('here');
-  //
-  // $('.workout-display').on('submit', function(event){
     event.preventDefault();
     let buttonId = $(event.target).find('.edit-workout').attr('data-id');
     let data = getFormFields(event.target);
@@ -65,9 +45,6 @@ const onEditWorkout = (event) => {
 const addHandlers = () => {
   $('#create-workout').on('submit', onCreateWorkout);
   $('#see-workouts').on('submit', onSeeWorkouts);//.on('click', addClassClicked);
-  // $('#find-workout').on('submit', onSeeWorkouts);//.on('click', addClassClicked);
-  // $('.delete-workout').on('click', onDeleteWorkout);
-  // $('.workout-display').on('submit', onEditWorkout);
 };
 
 module.exports = {
--- assets/scripts/auth/workouts_auth/workouts_ui.js
+++ assets/scripts/auth/workouts_auth/workouts_ui.js
@@ -22,23 +22,13 @@ const deleteWorkoutFailure = (error) => {
   console.error(error);
 };
 
-const editWorkoutSuccess = (data) => {
-//  console.log("Edit workout success app.user", app.user);
-//  app.user = data.user;
+const editWorkoutSuccess = () => {
     $("#see-workouts-button").click();
-  // api.seeWorkouts()
-  // .done(ui.seeWorkoutsSuccess)
-  // .fail(ui.seeWorkoutsFailure);
 };
 
 const deleteWorkoutSuccess = () => {
   console.log("Dete Workout Success appuser", app.user);
   $("#see-workouts-button").click();
-  //  events.onSeeWorkouts();
-  // console.log("Deleted!");
-  // api.seeWorkouts()
-  // .done(seeWorkoutsSuccess)
-  // .fail();
 };
 
 const editWorkoutFailure = (error) => {
@@ -46,47 +36,18 @@ const editWorkoutFailure = (error) => {
 };
 
 const findWorkoutByDate = (date, data) => {
-  console.log("workout date data:", date, data);
   $("#see-workouts-by-date").html("Here are your workouts on the date: " + index.findWorkoutInformation(date, data));
-};
-
-
-// $('.workout-display').on('submit', function(event){
-//   event.preventDefault();
-//   let buttonId = $(event.target).find('.edit-workout').attr('data-id');
-//   let data = getFormFields(event.target);
-//   data.workout.id = buttonId;
-//   data.workout.user_id = app.user.id;
-//   api.editWorkout(data)
-//   .done(editWorkoutSuccess)
-//   .fail();
-
-const refreshWorkouts = (data) =>{
-//  app.user.workouts = data.workouts;
-//console.log(app.user.workouts);
-console.log('refreshWOrkouts data', data);
-//  app.user.workouts = data.workouts;
-  $('#view-workouts').html('');
-  $('#view-workouts').html(workoutTemplate(app.user));
 };
 
 const onDeleteWorkout = (event) => {
 event.preventDefault();
-
 let buttonId = $(event.target).attr('data-id');
 api.deleteWorkout(buttonId)
 .done(deleteWorkoutSuccess)
 .fail(deleteWorkoutFailure);
-//console.log("App user post delete:", app.user);
-// refreshWorkouts();
-// api.seeWorkouts().done(deleteWorkoutSuccess);
 };
 
 const onEditWorkout = (event) => {
-  // event.preventDefault();
-  // console.log('here');
-  //
-  // $('.workout-display').on('submit', function(event){
     event.preventDefault();
     let buttonId = $(event.target).find('.edit-workout').attr('data-id');
     let data = getFormFields(event.target);
@@ -95,53 +56,19 @@ const onEditWorkout = (event) => {
     api.editWorkout(data)
     .done(editWorkoutSuccess)
     .fail();
-    // refreshWorkouts();
-    // api.seeWorkouts()
-    // .done(seeWorkoutsSuccess)
-    // .fail(seeWorkoutsFailure);
 };
-
-
 
 const seeWorkoutsSuccess = (data) => {
   app.user.workouts = data.workouts;
-//refreshWorkouts();
   $('#view-workouts').html('');
   $('#view-workouts').html(workoutTemplate(app.user));
   $('.delete-workout').on('click', onDeleteWorkout);
   $('.workout-display').on('submit', onEditWorkout);
-  // $('.delete-workout').on('click', function(event){
-  //   event.preventDefault();
-  //   let buttonId = $(event.target).attr('data-id');
-  //   api.deleteWorkout(buttonId)
-  //   .done(deleteWorkoutSuccess)
-  //   .fail(deleteWorkoutFailure);
-  // });
-
-  // $('.workout-display').on('submit', function(event){
-  //   event.preventDefault();
-  //   let buttonId = $(event.target).find('.edit-workout').attr('data-id');
-  //   let data = getFormFields(event.target);
-  //   data.workout.id = buttonId;
-  //   data.workout.user_id = app.user.id;
-  //   api.editWorkout(data)
-  //   .done(editWorkoutSuccess)
-  //   .fail();
-  //   event.preventDefault();
-  //
-  // });
-};
-
-const deleteWorkout = (event) => {
-  workoutEvents.onDeleteWorkout();
 };
 
 const seeWorkoutsFailure = (error) => {
   console.error(error);
 };
-
-
-
 
 module.exports = {
   createWorkoutSuccess,
